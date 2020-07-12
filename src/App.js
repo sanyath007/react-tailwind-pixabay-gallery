2f549b7a4908837d30719b15854e1ae35171b0c7
Sanyath Thammawong <sanyath007@gmail.com>
2020-07-12 10:23:36 +0700

    Clear unused files and modified App.js with showing simple image

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <img src="https://source.unsplash.com/random" alt="" className="w-full" />
+      <div className="px-6 py-4">
+        <div className="font-bold tex-purple-500 tex-xl mb-2">
+          Photo by Sanya
+        </div>
+      </div>
     </div>
   );
 }
