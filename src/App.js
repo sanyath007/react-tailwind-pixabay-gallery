0b1a17268e5657d89aa1f0b177e8e8789041dd66
Sanyath Thammawong <sanyath007@gmail.com>
2020-07-12 17:04:00 +0700

    Modified App.js with adding loading status

--- src/App.js
+++ src/App.js
@@ -18,11 +18,14 @@ function App() {
 
   return (
     <div className="container mx-auto">
-      <div className="grid grid-cols-3 gap-4">
-        {images.map(img => (
-          <ImageCard key={img.id} image={img} />
-        ))}
-      </div>
+      {isLoading 
+        ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1>
+        : <div className="grid grid-cols-3 gap-4">
+          {images.map(img => (
+            <ImageCard key={img.id} image={img} />
+          ))}
+        </div>
+      }
     </div>  
   );
 }
