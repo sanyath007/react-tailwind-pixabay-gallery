7cb18949bc28d58a63d8a106bc564514fbcf7a03
Sanyath Thammawong <sanyath007@gmail.com>
2020-07-12 10:35:19 +0700

    Created component draft in App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,32 @@ function App() {
         <div className="font-bold tex-purple-500 tex-xl mb-2">
           Photo by Sanya
         </div>
+        <ul>
+          <li>
+            <strong>Views :</strong>
+            4000
+          </li>
+          <li>
+            <strong>Downloads :</strong>
+            300
+          </li>
+          <li>
+            <strong>Likes :</strong>
+            600
+          </li>
+        </ul>
+      </div>
+
+      <div className="px-6 py-4">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #tag1
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #tag2
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #tag3
+        </span>
       </div>
     </div>
   );
